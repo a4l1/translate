9b88ab7854c5af0371a8c801f2ea0398d7441cbd
azizbek <120770336+a4l1@users.noreply.github.com>
2023-05-12 20:29:23 +0500

    tarjimon

---
+++ index.js
@@ -0,0 +1,142 @@
+
+let slovar = prompt("Meva yoki Sabzovot nomini kiriting")
+switch(slovar){
+    // mevalar
+    case "olma":
+        alert("яблоко")
+        alert("apple")
+        break;
+
+    case "uzum":
+        alert("виноград")
+        alert("Grapes")
+        break;
+
+        case "o'rik ":
+            alert("абрикос")
+            alert("apricot")
+            break;
+
+        case "zoldori":
+            alert("абрикос")
+            alert("apricot")
+            break;
+
+        case "anor":
+            alert("гранад")
+            alert("pomegranate")
+            break;
+
+        case "olcha":
+            alert("вишня")
+            alert("cherry")
+            break;
+
+        case "gilos":
+            alert("Черушня")
+            alert("Cherries")
+            break;
+
+        case "nok":
+            alert("Груша")
+            alert("Pear")
+            break;
+
+        case "malina":
+            alert("Малина")
+            alert("Raspberries")
+            break;
+
+        case "limon":
+            alert("Лимон")
+            alert("Lemon")
+            break;
+
+        case "qulupnay":
+            alert("клубника")
+            alert("Strawberries")
+            break;
+
+        case "tarbuz":
+            alert("Арбуз")
+            alert("Watermelon")
+            break;
+
+        case "apelsin":
+            alert("Апельсин")
+            alert("Orange")
+            break;
+
+        case "banan":
+            alert("Банан")
+            alert("Banana")
+            break;
+        // sabzavotlar
+        case "kartoshka":
+            alert("Картофель")
+            alert("potatoes")
+            break;
+
+        case "piyoz":
+            alert("лук")
+            alert("onion")
+            break;
+
+        case "karam":
+            alert("капуста")
+            alert("cabbage")
+            break;
+            
+        case "pomidor":
+            alert("помидор")
+            alert("tomato")
+            break;
+
+        case "bodring":
+            alert("огурэц")
+            alert("cucumber")
+            break;
+
+        case "balgar":
+            alert("балгарски")
+            alert(" bell peper")
+            break;
+
+            case "makkajo'xori":
+                alert("Кукуруза")
+                alert("corn")
+                break;
+
+
+                case "qovoq":
+                    alert("Цуккини")
+                    alert("eyelid")
+                    break;
+
+                case "ismaloq":
+                    alert("Шпинат")
+                    alert("spinach")
+                    break;
+
+                case "turp":
+                    alert("Редис")
+                    alert("horse")
+                    break;
+
+                case "qo'ziqorin":
+                    alert("Гриб")
+                    alert("mushroom")
+                    break;
+
+                case "baqlajon":
+                    alert("баклажан")
+                    alert("egg-plant")
+                    break;
+
+
+
+
+        default:
+            alert("bu hato")
+           
+}
